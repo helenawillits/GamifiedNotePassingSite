e776a25b559fd2d743b204c077825590190ba1b1
Helena Willits <helenawillits@MacBookAir.attlocal.net>
2026-02-09 15:58:52 -0800

    Fix site path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/GamifiedNotePassingSite" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/GamifiedNotePassingSite/" : "",
   typescript: {
     ignoreBuildErrors: true,
   },
